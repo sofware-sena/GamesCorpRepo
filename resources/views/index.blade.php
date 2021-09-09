087ccb974b86f8d6be107a8dc3bc2874f802e29f
Moises David Montenegro Guerra <dmoises084@gmail.com>
2021-09-09 18:00:17 -0500

    Title -_-

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>®it'sGamesCorp®®</title>
+    <title>GamesCorp®</title>
     
     <!-- favicon -->
      <link rel=icon href="assets/img/icono.png" sizes="200x499" type="image/png"> 
